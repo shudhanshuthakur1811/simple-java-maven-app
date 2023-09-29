5b29baa220ddce2ed5bc5ab11d6ecabe68aa1c05
shudhanshuthakur1811 <shudhanshu.thakur1811@gmail.com>
2023-09-29 14:30:51 +0530

    first commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,12 +5,12 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGE = "Hello World!!!!!!";
 
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println(MESSAGE);
+        System.out.println(MESSAGE !!!);
     }
 
     public String getMessage() {
