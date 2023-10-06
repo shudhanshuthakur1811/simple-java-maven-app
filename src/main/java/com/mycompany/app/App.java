18751e2948561535ca5565a03a84124623ed39fb
shudhanshuthakur1811 <shudhanshu.thakur1811@gmail.com>
2023-10-06 12:34:08 +0530

    new commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World  12311!!!!";
+    private static final String MESSAGE = "Hello World  11!!!!";
 
     public App() {}
 
